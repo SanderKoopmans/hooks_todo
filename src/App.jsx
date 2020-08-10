1ba59a510af41b70fc086af8c650565c71f3207c
Sander Koopmans <42515367+SanderKoopmans@users.noreply.github.com>
2020-08-10 23:14:02 +0200

    framework done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import AddForm from './components/AddForm';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({
+    id: 0,
+    item: 'Practise using hooks',
+  });
+
+  const onSubmit = event => {
+    event.preventDefault();
+    console.log('onSubmit fired');
+  };
+
   return (
     <div className="App">
       <h1>Hooks todo</h1>
-      <AddForm />
+      <form>
+        <label htmlFor="todo-add">
+          Add new todo item
+          <input
+            id="todo-add"
+            name="todo-add"
+            placeholder="Add something" />
+        </label>
+        <button type="submit" onClick={onSubmit}>Add item</button>
+      </form>
+      <h3>PLaceholder for list items</h3>
     </div>
   );
 }
