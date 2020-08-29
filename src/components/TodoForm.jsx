854d259aa12488dbeabd05bbd764dcb21c3df1b8
Sander Koopmans <sandkoop@gmail.com>
2020-08-29 23:07:09 +0200

    Begin Mui styling

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,25 +1,32 @@
 import React, { useState } from 'react';
+import { 
+  Button,
+  Input,
+  InputLabel,
+} from '@material-ui/core';
 
 const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState('');
 
   return (
-    <form onSubmit={e => {
+    <form
+      onSubmit={e => {
       e.preventDefault();
       addTodo(value);
       setValue('');
     }}>
 
-      <label htmlFor="input">
-        Add new item:
-        <input
+      <InputLabel htmlFor="input">
+        Add item:
+        <Input
           name="input"
           type="text"
           value={value}
           onChange={e => { setValue(e.target.value)}}
-          placeholder="Add new item..." />
-      </label>
-      <button type="submit">Add item</button>
+          placeholder="Add new item..." 
+          inputProps={{ 'aria-label': 'description' }} />
+      </InputLabel>
+      <Button type="submit" variant="contained" color="primary">Add item</Button>
     </form>
   )
 };
